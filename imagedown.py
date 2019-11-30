ffec9e6a15e66ee68b233532051074b333ee3b12
Kushal Poudel <kushalpoudel35@gmail.com>
2019-11-30 10:36:36 +0545

    few edits

--- imagedown.py
+++ imagedown.py
@@ -3,9 +3,11 @@ import urllib.request
 def download_web_image(image_url):
 	name = input("Enter the name you want to save the image as: ")
 	fullname = str(name) + ".jpg"
+	print("Downloading " + fullname + " ...")
 	urllib.request.urlretrieve(image_url, fullname)
+	print(fullname + " downloaded.")
 
 url = str(input("Hi there! This is a program that lets you download an image from the web.\nEnter the url of the image: "))
 
 download_web_image(url)
-input("Your image has been downloaded. Press any key to exit...")
+input("Press any key to exit...")
